7f80d57d3d197fe5053ee9a22f39d448373cd17d
Mohammed A <am18_8@outlook.com>
2023-10-10 16:38:26 +0300

    add practice.dart

---
+++ practice.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+void main() {
+  num? age;
+  num? number;
+  //  Write a Dart program that takes `name` and `age` as input from a user, and if `age` is less than 18, print a message.
+  print("inter your name: ");
+  String? name = stdin.readLineSync();
+  print("enter your age: ");
+  bool HasAge = false;
+  do {
+    try {
+      age = num.parse(stdin.readLineSync()!);
+      HasAge = true;
+    } catch (error) {
+      print("this is an error please enter the correct number:");
+    }
+  } while (!HasAge);
+  if (age! <= 18) {
+    print("Sorry, you are under 18 years old");
+  } else {
+    print("You are adult");
+  }
+  // Write a Dart program that takes a `number` from a user, and prints the cube from 1 to the given `number` using a loop.
+  print("enter a number please: ");
+  bool Hasnumber = false;
+  do {
+    try {
+      number = num.parse(stdin.readLineSync()!);
+      Hasnumber = true;
+    } catch (error) {
+      print("this is an error please enter a valid number:");
+    }
+  } while (!Hasnumber);
+  {
+    for (int i = 1; i <= number!; i++) {
+      num cube = i * i * i;
+      print("number is :$i cube is $cube");
+    }
+  }
+}
